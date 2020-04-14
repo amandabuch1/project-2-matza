79cd7e50ea09e86503ecd392c8a3b237a95ead87
Amandabuch <amandalbuch@gmail.com>
2020-04-14 15:40:36 -0700

    Able to create a comment in api

--- controllers/api/comments.js
+++ controllers/api/comments.js
@@ -4,8 +4,6 @@ const Matza = require('../../models/matza');
 function create(req, res) {
     Matza.findById(req.params.id, function(err, matza) {
       console.log("console.log(matza)", matza);
-      console.log("console.log(req.params.id)", req.params.id);
-
       res.send(matza);
     
       

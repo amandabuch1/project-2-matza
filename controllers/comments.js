37ad8051e63aeba12f51679b870ec6b788008622
Amandabuch <amandalbuch@gmail.com>
2020-04-27 09:49:08 -0700

    Able to delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -12,6 +12,23 @@ const create =(req, res)=>{
   })
 }
 
+
+const delComment = (req, res) =>{
+  console.log(req.params);
+  Matza.findById(req.params.matzaid)
+  .exec(function(err, matza){
+    // https://mongoosejs.com/docs/2.7.x/docs/embedded-documents.html
+    // individual matza. comments array in matza. id(specific comment in array. remove removes it when button is clicked )
+    matza.comments.id(req.params.commentid).remove();
+    // Save updated matza and redirect to the same page
+    matza.save(function(err) {
+      res.redirect(`/matzas/${matza._id}`);
+    });
+  })
+  
+};
+
 module.exports = {
   create,
+  delComment,
 }
--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var commentsCtrl = require('../controllers/comments');
 
 router.post('/matzas/:id/comments',isLoggedIn, commentsCtrl.create);
+router.delete('/matzas/:matzaid/comments/:commentid', commentsCtrl.delComment);
 
 function isLoggedIn(req, res, next) {
     if ( req.isAuthenticated() ) return next();
